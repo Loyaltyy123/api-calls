6ec695825fe957a058b9a077f43a1405cacb12c9
Loyaltyy123 <loyaltytrainings@gmail.com>
2025-07-29 15:29:38 +0530

    code

--- src/components/Master.jsx
+++ src/components/Master.jsx
@@ -1,6 +1,14 @@
+import DeleteComp  from "./LMPL0262/DeleteComp";
+import GetComp from "./LMPL0262/GetComp";
+import PostComp from "./LMPL0262/PostComp";
+import PutComp from "./LMPL0262/PutComp";
 const Master =()=>{
     return(
        <>
+        <GetComp></GetComp>
+        <PostComp></PostComp>
+        <PutComp></PutComp>
+        <DeleteComp></DeleteComp>
        </>
     )
 
