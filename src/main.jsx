e59bc57a267366986430e5be5c6b8a586de38153
Rajeshwari-11 <gunagantirajeshwari1@gmail.com>
2025-07-29 15:16:55 +0530

    62 updated

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Master from './components/Master.jsx'
-import GetComp from './components/LMPL0262/GetComp.jsx'
-import PostComp from './components/LMPL0262/PostComp.jsx'
-import PutComp from './components/LMPL0262/PutComp.jsx'
-import DeleteComp from './components/LMPL0262/DeleteComp.jsx'
+ 
 
 createRoot(document.getElementById('root')).render(
-     <DeleteComp></DeleteComp>
+   
 )
