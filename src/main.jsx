a937ce9757e55525536bfce5949b1c1b758361a1
Loyaltyy123 <loyaltytrainings@gmail.com>
2025-07-29 15:14:04 +0530

    code

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-
+import ExpensiveCalculationComponent from "./components/ExpensiveCalculationComponent.jsx";
+import UseCallbackExample from './components/UseCallbackExample.jsx';
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <UseCallbackExample></UseCallbackExample>,
 )
